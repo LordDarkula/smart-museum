bb2a8fe04f93bc86015b15d3ee6d8cd20ffcda27
Aubhro Sengupta <aubhros@gmail.com>
2020-02-29 15:13:13 -0500

    start update endpoint

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,3 +1,5 @@
+from os import listdir
+from os.path import isfile, join
 import os
 import shutil
 import json
@@ -27,7 +29,8 @@ def all():
 
 @app.route('/image')
 def image():
-    return send_file('current.jpg')
+    onlyfiles = [f for f in listdir(mypath) if isfile(join('current', f))]
+    return send_file(onlyfiles[0])
 
 @app.route('/start/<museum>/<collection>')
 def start(museum, collection):
@@ -41,4 +44,38 @@ def start(museum, collection):
 
     with open('image_data.json', 'w') as fp:
         json.dump(images, fp)
+    
+    if os.path.exists('current'):
+        shutil.rmtree('current')
+    os.makedirs('current')
+    shutil.copyfile(os.path.join('images', 'image1.jpg'), os.path.join('current', 'image1.jpg'))
+    with open('current.txt', 'w') as f:
+        f.write('image1')
+    
     return "Museum {}, collection {}".format(escape(museum), escape(collection))
+
+@app.route('/update/<direction>')
+def update(direction):
+    if not os.path.exists('visited.json'):
+        with open('visited.json', 'w') as f:
+            json.dump({}, f)
+
+
+    with open('visited.json', 'r') as f:
+        visited = json.load(f)
+        
+        with open('current.txt') as f:
+            current_name = f.readline()
+            with open('image_data.json', 'r') as f:
+                image_data = json.load(f)
+
+                for key, val in image_data:
+                    if key == current or key in visited:
+                        continue
+
+                    
+
+            for key, val in image_data:
+
+
+
