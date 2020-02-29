e081db1001f7223ad6260b2f73015c5e80c7aba2
Aashish Thoutam <aashish.thoutam@gmail.com>
2020-02-29 17:10:32 -0500

    Confirmation Modal on Frontend

---
+++ frontend/src/components/ConfirmationModal.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../style/App.css";
+
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+
+function ConfirmationModal(props) {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  let toggleModal = _ => {
+    setIsModalOpen(!isModalOpen);
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <Modal isOpen={isModalOpen} toggle={toggleModal} size="xl">
+        <ModalBody>
+          <h1>{props.success ? "Your VR Experience is Ready" : "Uh oh ..."}</h1>
+          <p>
+            {props.success
+              ? "Place the phone in the Google Cardboard and interact with the gesture detector."
+              : "There was an error creating your VR experience. Please check your network connection."}
+          </p>
+          {/* TODO: Throw in an image of VR person PNG */}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleModal}>
+            Ok
+          </Button>{" "}
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default ConfirmationModal;
