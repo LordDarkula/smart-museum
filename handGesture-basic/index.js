9786d0a4bec0e7c8ed268fff5b28b1c5d66fa34f
Aashish Thoutam <aashish.thoutam@gmail.com>
2020-02-29 13:22:36 -0500

    Handtrack JS

---
+++ handGesture-basic/index.js
@@ -0,0 +1,52 @@
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let isVideo = false;
+let model = null;
+
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.6 // confidence threshold for predictions.
+};
+
+function startVideo() {
+  handTrack.startVideo(video).then(function(status) {
+    console.log("video started", status);
+    if (status) {
+      isVideo = true;
+      runDetection();
+    } else {
+      updateNote.innerText = "Please enable video";
+    }
+  });
+}
+
+function runDetection() {
+  model.detect(video).then(predictions => {
+    processPredictions(predictions);
+    model.renderPredictions(predictions, canvas, context, video);
+    if (isVideo) {
+      requestAnimationFrame(runDetection);
+    }
+  });
+}
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+  // detect objects in the image.
+  model = lmodel;
+  updateNote.innerText = "Loaded Model!";
+  trackButton.disabled = false;
+});
+startVideo();
+
+let isCalibrated = false;
+
+let processPredictions = predictions => {
+  if (predictions.length > 0) {
+    console.log("Predictions: ", predictions);
+  }
+};
