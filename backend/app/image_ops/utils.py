fe7d3b7fb6282da5a827e85c007612eb2b8a5469
Aubhro Sengupta <aubhros@gmail.com>
2020-02-29 17:34:44 -0500

    Change to requests

--- backend/app/image_ops/utils.py
+++ backend/app/image_ops/utils.py
@@ -1,11 +1,10 @@
-import urllib.request
 import ssl
+import requests
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
 def download_image_with_url(url: str, output_name: str):
-    urllib.request.urlretrieve(url, output_name + ".jpg")
-
+    r = requests.get(url, allow_redirects=True)
+    open(output_name + '.jpg', 'wb').write(r.content)
 
 if __name__ == '__main__':
 
